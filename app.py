dde131140e0aec39bee11c1c44a86f91159c931e
FAIZAAN ALI BAIG <97731707+faizaanalibaig@users.noreply.github.com>
2022-07-21 22:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import numpy as np
 import streamlit as st
 import cv2
 import pytesseract
-import PIL import Image # python Imaging library to open images
-import numpy as np
-
+from PIL import Image # python Imaging library to open images
+pytesseract.pytesseract.tesseract_cmd = '/app/ apt/usr/bin/tesseract'
+st.set_option('deprecation.showfileUploaderEncoding', False) # to warning ignore
 st.title("Optical character recognition (OCR) :")
 st.text("Upload the image")
 
 uploaded_file = st.sidebar.file_uploader("choose an image :", type = ["jpg","png","jpeg"])
 if uploaded_file is not None: # if there is some file uploaded here, then do the following 
   img = Image.open(uploaded_file) # reads the file uploaded (similar to cv2.imread)
+  st.image(img, caption='Uploaded Image',use_column_width=True) #displays image, caption is uploaded image, uses image width
   st.write("")
   if st.button("PREDICT"):
     st.write("Resrlt :")
-    info = pytesseract.image_to_string(img) # ocr is applied using pytesseract
+    info = pytesseract.image_to_string(img) # ocr is applied using pytesseract # pytesseract img to text and prints
     st.title(info)
